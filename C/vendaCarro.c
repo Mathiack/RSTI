b12f698b71ef67eb326c05c9d4b49b16848a6ce2
Thunder <151688223+Guilherme-Thunder@users.noreply.github.com>
2024-05-29 17:11:23 +0000

    vendaCarro

---
+++ C/vendaCarro.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    float valorCusto, valImpostoPer = 0.45, valDistribuidoraPer = 0.12;
+    float valorImp, valorDist, valorFinal;
+    
+    printf("Preco do carro: ");
+    scanf("%f", &valorCusto);
+    
+    valorImp = valorCusto * valImpostoPer;
+    valorDist = valorCusto * valDistribuidoraPer;
+    valorFinal = valorCusto + valorDist + valorImp;
+    
+    printf("Preço: %f ",  &valorCusto);
+    printf("Imposto: %f ", &valorImp);
+    printf("Distribuidora: %f ", &valorDist);
+    printf("Valor final: %f ", &valorFinal);
+    return 0;
+}
