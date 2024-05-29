6b02fd59bd771898a92672dbe71b5265a3289114
Thunder <151688223+Guilherme-Thunder@users.noreply.github.com>
2024-05-29 18:56:41 +0000

    vogais numeros e operador aritimetico

---
+++ C/vogalNumeroOperador.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+//NÃO TERMINADO
+int main() {
+    char coisa;
+    
+    printf("Bota alguma coisa: \n");
+    scanf("%f", &coisa);
+    
+    switch(coisa) {
+        case 'A':
+            printf("Vogal maiuscula");
+            break;
+        case 'E':
+            printf("Vogal maiuscula");
+            break;
+        case 'I':
+            printf("Vogal maiuscula");
+            break;
+        case 'O':
+            printf("Vogal maiuscula");
+            break;
+        case 'U':
+            printf("Vogal maiuscula");
+            break;
+        case 0:
+            printf("Número de 0 a 9");
+            break;
+        case 1:
+            printf("Número de 0 a 9");
+            break;
+        case 2:
+            printf("Número de 0 a 9");
+            break;
+        case 3:
+            printf("Número de 0 a 9");
+            break;
+        case 4:
+            printf("Número de 0 a 9");
+            break;
+        case 5:
+            printf("Número de 0 a 9");
+            break;
+        case 6:
+            printf("Número de 0 a 9");
+            break;
+        case 7:
+            printf("Número de 0 a 9");
+            break;
+        case 8:
+            printf("Número de 0 a 9");
+            break;
+        case 9:
+            printf("Número de 0 a 9");
+            break;
+        case '+':
+            printf("");
+            break;
+        case '-':
+            printf("");
+            break;
+        case '*':
+            printf("");
+            break;
+        case '/':
+            printf("");
+            break;
+        default: 
+            printf("pode não man");
+    }
+    return 0;
+}
