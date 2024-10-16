745dd4a005f6f603fe7bf92aae2585ec07fce36d
Mathiack <guilhermemathiack@gmail.com>
2024-10-16 14:08:10 -0300

    novas coisas
    so commitei agora por que nao lembrei

---
+++ RSTI_2/JavaScript/basico/ola.js
@@ -0,0 +1,10 @@
+let nome = "Guilherme"
+let idade = 15
+
+console.log("Meu nome é " + nome + " e tenho " + idade + " anos.")
+
+if (idade >= 18) {
+    console.log("Sou maior de idade.")
+} else {
+    console.log("Sou menor de idade, pois tenho " + idade + " anos.")
+}
